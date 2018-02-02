11fffabcce78907378ac353f0d759964f7985aec
Ryan Burgett <35535754+Ryan-Burgett@users.noreply.github.com>
2018-02-02 10:31:11 -0600

    Update IntCycle.c

--- assignment5/IntCycle.c
+++ assignment5/IntCycle.c
@@ -1,19 +1,3 @@
-/*         Ryan Burgett
-        rdb@iastate.edu 
-        CPRE 185 Section L  
-Programming Practice NUMBER 5
-<Reflection 1 What were you trying to learn or achieve?>
-                I was trying to create a program that cycles through a given set of numbers using pointers.
- <Reflection 2 Were you successful? Why or Why not?>
-        I was successful. My program will cycle through a set of three integers.
-<Reflection 3 Would you do anything differently if starting this program over?  If so, explain what.>
-        I would make my program able to cycle through a chosen number of integers not just three.
-<Reflection 4 Think about the most important thing you learned when writing this piece of code.  What was it and explain why it was significant.>
-        I learned that if you use pointers as parameters you have to use pointers throughout the entire function.
-<Other questions/comments for Instructors>
-        Answer here (optional)
-*/
-
 #include<stdio.h>
 
 void cycle(int *a, int *b, int *c);
